c4029321f747e696df69417398e0030cff03689f
gvfirmeza <pmvieira@outlook.com>
2023-11-16 21:24:09 -0300

    ap2

---
+++ AP2/segmentation.py
@@ -0,0 +1,4 @@
+from totalsegmentator.python_api import totalsegmentator
+
+if __name__ == "__main__":
+    totalsegmentator("C:/Users/pmvie/OneDrive/Área de Trabalho/scrr/example_ct_dicom", "C:/Users/pmvie/OneDrive/Área de Trabalho/scrr/uploads")
